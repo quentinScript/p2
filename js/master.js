1953447e1bc99885e5924ddeaf41667f7db18c50
BOZON Quentin <quentin.bozon1@gmail.com>
2020-11-08 12:59:22 +0100

    v.1 Premier rendu

--- js/master.js
+++ js/master.js
@@ -2,6 +2,7 @@ let nomActeurs = [];
 let valTime= 0;
 let inputActor = false;
 let actorCheck = 0;
+let actorVerif = false;
 
 
 $(document).ready(function(){
@@ -9,8 +10,10 @@ $(document).ready(function(){
     $(".toggleTimeline").click(timelineShow);
     // reduire la taille d'une classe dans l'inspecteur
     $("[data-toggle-show]").click(toggleShowInspecteur);
+
+
     // acteur choix ou non
-    $("[data-button]:not([data-button='add'])").click(buttonActor);
+    // $("[data-button]:not([data-button='add'])").click(buttonActor);
     sliderRange();
     //share
     $('.headerDroite [data-button],#close, .full').click(overlay);
@@ -21,6 +24,7 @@ $(document).ready(function(){
     $('[data-rect]').hover(rectHover);
     // ajout acteurs
     $('.hovAdd').mouseenter(mouseEnterActor).mouseleave(mousLeaveActor);
+    $('[data-changeInput]').hover(changeInput);
     // ajouter un acteur avec bouton check ou la touche entrer
     $('.check').click(addActor);
     $("#actor").on('keyup', function (e) {
@@ -28,17 +32,46 @@ $(document).ready(function(){
             addActor();
         }
     });
+    // toggle actor tracklet / preparation
+    $('#actorToggle').click(toggleActor);
+    // activer / désactiver un acteurs dans la partie edit
+    $('[data-num]').click(toggleA);
+
     //menu
     $('#edit').click(edit);
     $('#preparation').click(preparation);
 
+    //précoupage acteurs
+    $('#togglePredecoupage').click(function(){
+        $('#montage, #montageVide').toggleClass('none');
+    })
+    //tracklet Suivant
+    $('#prepaSuivant').click(function(){
+        if (actorVerif == true) {
+            $('.montagePerso').removeClass('none');
+            $('#imgPrepa').addClass('show');
+            $('[data-rect]').addClass('none');
+            $('#popUpSuivant').addClass('hide');
+        }
+    })
+
+
+
     //On attend .3s avant d'exécuter d'afficher preparation car sinon le crop d'imagebug
-    setTimeout(preparation, 300);
+    setTimeout(preparation, 400);
     // popup
     setTimeout(function(){
         $('#popUpAddActor').removeClass("hide");
     }, 1000);
+
 });
+
+function toggleA(){
+    // $('#actorPrepa:first-child').addClass('show');
+    let select = $(this).data('num');
+
+    $('[data-num="'+select+'"]').toggleClass('border');
+}
 
 function mousLeaveActor(){
     // si input vide reset sinon il reste visible
@@ -55,14 +88,14 @@ function mouseEnterActor(){
     // si l'input est vide
     if (inputActor == false) {
         $('[data-button="add"]').toggleClass("show");
-        $('.check').toggleClass('show');
+        $('#checkAdd').toggleClass('show');
         $('.hovAdd input').toggleClass('show');
     }
 }
 
 function addActor(){
     let saisie = document.getElementById('actor').value;
-    let close = "<div data-button='0'><span class='iconify' data-inline='false' data-icon='gg:close'style='background-color:#E4E1E8; border-radius: 2px;color: #628C6E;cursor:pointer'></span></div>";
+    let close = "<span class='iconify' data-inline='false' data-icon='gg:close'style='background-color:#E4E1E8; border-radius: 2px;color: #628C6E;cursor:pointer'></span>";
     if (saisie != "") {
 
         nomActeurs.push(saisie);
@@ -70,11 +103,15 @@ function addActor(){
         let htmlIspecteur="";
         for (var i = 0; i < nomActeurs.length; i++) {
             html += "<div>"+nomActeurs[i]+"</div>";
-            htmlIspecteur += "<div data-button='0'>"+nomActeurs[i]+"</div>";
+            // console.log(i);
+            htmlIspecteur += "<div data-button='0' data-num='"+i+"'>"+nomActeurs[i]+"</div>";
+            // html += "<div class='relative' style='height:40px' data-changeInput><div data-button='0' class='absolute' style='z-index:2'><input type='text' value='"+nomActeurs[i]+"' data-input></div><div class='check' data-remove><span class='iconify' data-inline='false' data-icon='gg:close' style='color: #e4e1e8; font-size: 19px;margin: 4px;'></span></div></div>"
         }
+        let objActor = $(htmlIspecteur);
+        objActor.click(toggleA);
         $('.timePerso').html(html);
         $('#actors').html(htmlIspecteur);
-        $('[data-show="0"]').html(htmlIspecteur);
+        $('#actorPrepa').html(objActor);
         //rect assigne
         $('[data-rect]').html(close + htmlIspecteur);
         $(".hovAdd").addClass("plus");
@@ -82,15 +119,26 @@ function addActor(){
     }
 }
 
+function changeInput(){
+    $('#actors [data-button]').toggleClass('show');
+    $('[data-remove]').toggleClass('show');
+}
+
+function toggleActor(){
+    $('#actorTracklet, #actorPrepa').toggleClass('none');
+}
+
 function rectHover(){
     let select = $(this).data('rect');
+    console.log(select);
     $('[data-rect="'+select+'"] [data-button]').toggleClass('hide');
     // au click afficher les != actors
-    $('[data-rect] [data-button]').click(function rect(){
+    $('[data-rect] [data-button], [data-rect] .iconify').click(function rect(){
         $('[data-rect="'+select+'"]').addClass('ok');
         //si tous acteurs ont été assigné  > popup moment suivant
         if ($('[data-rect="0"]').hasClass('ok') && $('[data-rect="1"]').hasClass('ok')) {
             $('#popUpSuivant').removeClass('hide');
+            actorVerif = true;
         }
     });
 }
@@ -205,18 +253,27 @@ function edit(){
     $('#apercuEditGlobal').removeClass('none');
     $('#popUpSuivant').addClass('hide');
     $('#prepaSuivant').addClass('none');
-
+    $('.timePerso').addClass('edit');
+    $('.tools .prepa').removeClass('none');
+    $('.montagePerso').addClass('edit');
+    if( $('input[id=togglePredecoupage]').is(':checked') ){
+        $('#montage').removeClass('none');
+    } else {
+        $('#montageVide').removeClass('none');
+    }
 }
 
 function preparation(){
     $("#preparation").addClass("show");
     $("#edit").removeClass("show");
-    $('.timeSound').addClass("prepa");
-    $('.timePerso').addClass("prepa");
+    $('.timeSound, .timePerso').addClass("prepa");
     $('.inspecteur .block:not(.prepa)').css('display','none');
     $('.inspecteur .block.prepa').css('display','block');
     $('#apercuPrepa').removeClass('none');
     $('#apercuEditGlobal').addClass('none');
     $('#prepaSuivant').removeClass('none');
+    $('.timePerso, .montagePerso').removeClass('edit');
+    $('#montage, #montageVide').addClass('none');
+    $('.tools .prepa').addClass('none');
 
 }
